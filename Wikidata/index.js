83dad040961c13d2961202b35eedcffb1e358763
EstevesAndre <estevesandre1@hotmail.com>
2020-10-06 20:11:52 +0100

    Implement wikidata multiple requests

--- Wikidata/index.js
+++ Wikidata/index.js
@@ -1,92 +1,140 @@
 const fetch = require("node-fetch");
+const wdk = require("wikidata-sdk");
 const diseases = require("./diseases.json");
 const options = require("./options.json");
 
 const URL = "https://www.wikidata.org/w/api.php";
 
+const SEP = "%7C";
+const LANGUAGES = options.languages.join(SEP);
+const PROPS = ["labels" + SEP + "descriptions" + SEP + "claims", "labels"];
+
 var result = [];
 
-const parseJSON = (response) => {
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const fetchAllData = async () => {
+  for (var i = 0; i < diseases.length; i++) {
+    await fetchWikiData(diseases[i], 0);
+    if (i != diseases.length - 1) {
+      console.log("Waiting 6 seconds until next request!");
+      await sleep(6000);
+    }
+  }
+
+  console.log(result);
+};
+
+const getURL = (IDs, propType) => {
+  return (
+    URL +
+    "?action=wbgetentities&ids=" +
+    IDs +
+    "&format=json" +
+    "&languages=" +
+    LANGUAGES +
+    "&props=" +
+    PROPS[propType]
+  );
+};
+
+const fetchWikiData = async (ID, propType) => {
+  const URL = getURL(ID, propType);
+  console.log(URL);
+  const response = await fetch(URL);
+  const myJSON = await response.json(); //extract JSON from the http response
+
+  await parseJSON(myJSON);
+};
+
+// fetchWikiData(diseases[0], 0);
+fetchAllData();
+
+const parseJSON = async (response) => {
   let obj = new Object();
 
   obj.id = Object.keys(response.entities)[0];
-
   const entity = response.entities[obj.id];
 
-  getOptionQueries(entity, obj);
+  parseOptionQueries(entity, obj);
 
   for (let i = 0; i < options.properties.length; i++) {
-    console.log(options.properties[i]);
-    getClaim(entity.claims, options.properties[i].code)
-      .then((result) => parseResponse(result, obj, options.properties[i].name))
-      .catch((err) => console.log(err));
-    // break;
+    await getPropertyData(entity.claims, options.properties[i], obj);
+    await sleep(Math.floor(Math.random() * 100));
   }
 
-  console.log(obj);
+  result.push(obj);
 };
 
-const parseResponse = (result, obj, child) => {
-  obj[child] = new Object();
-  const query = options.queries[0];
-  console.log(result);
-
-  for (let j = 0; j < options.languages.length; j++) {
-    var opt = [];
-
-    for (let i = 0; i < result.length; i++) {
-      const labels =
-        result[i].entities[Object.keys(result[i].entities)[0]][query];
-
-      opt.push(...[labels[options.languages[j].code]]);
-    }
-
-    obj[child][options.languages[j].name] = opt;
-  }
-
-  // console.log(obj);
-};
-
-const getOptionQueries = (entity, obj) => {
+const parseOptionQueries = (entity, obj) => {
   for (let i = 0; i < options.queries.length; i++) {
     const query = options.queries[i];
-    var opt = [];
-    for (let j = 0; j < options.languages.length; j++) {
-      opt.push(...[entity[query][options.languages[j].code]]);
-    }
-    obj[query] = opt;
+    obj[query] = entity[options.queries[i]];
   }
 };
 
-const getClaim = async (claims, key) => {
+const getPropertyData = async (claims, property, obj) => {
+  try {
+    var prop = await requestProperty(claims, property.code);
+  } catch (err) {
+    console.log(err);
+  }
+
+  obj[property.name] = [];
+
+  Object.keys(prop.entities).map((key) =>
+    obj[property.name].push({
+      id: prop.entities[key].id,
+      labels: prop.entities[key].labels,
+    })
+  );
+};
+
+const requestProperty = async (claims, key) => {
   if (claims.hasOwnProperty(key)) {
-    console.log(claims[key].length);
-    let promiseArray = [];
+    var ids = "";
+
     for (let i = 0; i < claims[key].length; i++) {
       const obj = claims[key][i];
-      const id = obj.mainsnak.datavalue.value.id;
-      promiseArray.push(
-        fetch(getURL(id))
-          .then((response) => response.json())
-          .catch((err) => console.log(err))
-      );
+      ids +=
+        obj.mainsnak.datavalue.value.id +
+        (i == claims[key].length - 1 ? "" : SEP);
     }
-    return await Promise.all(promiseArray);
+
+    console.log("REQUEST: " + getURL(ids, 1));
+
+    return await fetch(getURL(ids, 1))
+      .then((response) => response.json())
+      .catch((err) => console.log(err));
   }
 };
 
-const getURL = (ID) => {
-  return URL + "?action=wbgetentities&ids=" + ID + "&format=json";
+const fetchTest = () => {
+  const ids = [
+    "Q30141616",
+    "Q30141302",
+    "Q30141343",
+    "Q30141359",
+    "Q30141517",
+    "Q30141399",
+    "Q30141432",
+    "Q30141385",
+    "Q30141414",
+    "Q30141588",
+    "Q83030",
+  ];
+
+  const url = wdk.getEntities({
+    ids: ids,
+    languages: ["en", "fr"],
+    format: "json",
+    props: ["info", "claims"],
+    redirections: true, // defaults to true
+  });
+
+  console.log(url);
 };
 
-const fetchWikiData = async (ID) => {
-  const response = await fetch(getURL(ID));
-  const myJSON = await response.json(); //extract JSON from the http response
-  parseJSON(myJSON);
-};
-
-// for (var i = 0; i < diseases.length; i++) {
-//   fetchWikiData(diseases[i]);
-// }
-
-fetchWikiData(diseases[0]);
+// fetchTest();
