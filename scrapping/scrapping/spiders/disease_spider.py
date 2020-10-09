a1414da84880903394e5cfbfbbcc87ad0042a62b
helenaMontenegro <up201604184@fe.up.pt>
2020-10-09 15:37:46 +0100

    Add scrapping

---
+++ scrapping/scrapping/spiders/disease_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+from scrapy.selector import Selector
+from bs4 import BeautifulSoup
+import re
+
+
+class DiseaseSpider(scrapy.Spider):
+    name = "disease"
+
+    def start_requests(self):
+        urls = [
+            'https://en.wikipedia.org/wiki/Alzheimer%27s_disease',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        heading = response.css('#firstHeading::text').get()
+        body = response.css('#mw-content-text table + p, #mw-content-text table + p + p').extract()
+        soup = BeautifulSoup(body[0])
+        overview = soup.get_text().encode("utf-8")
+        overview = re.sub(r'\[.+?\]', '', overview)
+        soup = BeautifulSoup(body[1])
+        paragraph2 = soup.get_text().encode("utf-8")
+        paragraph2 = re.sub(r'\[.+?\]', '', paragraph2)
+        overview += paragraph2
+        print("\n\nheading: " + heading + "\n\n")
+        print("\n\nbody: " + overview + "\n\n")
+
