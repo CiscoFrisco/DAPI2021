e7eec19ae3e361cecba64fab646682bca05603ca
helenaMontenegro <up201604184@fe.up.pt>
2020-10-10 23:48:09 +0100

    Add code to json file

--- scrapping/scrapping/spiders/disease_spider.py
+++ scrapping/scrapping/spiders/disease_spider.py
@@ -8,20 +8,23 @@ import os
 class DiseaseSpider(scrapy.Spider):
     name = "disease"
     diseases = []
+    disease_urls = []
+    f = ''
+    num = 0
 
     def start_requests(self):
-        disease_urls = read_from_json()
-        for disease in disease_urls:
+        self.disease_urls = read_from_json()
+        
+        self.f = open("../disease_overviews.json", "w")
+        self.f.write('[')#json.dumps(self.diseases, indent=4, separators=(", ", ": "))
+        for disease in self.disease_urls:
             yield scrapy.Request(url=disease["wikipediaURL"], callback=self.parse)
-        print(len(self.diseases))
-        f = open("../disease_overviews.json", "w")
-        f.write(json.dumps(self.diseases, indent=4, separators=(", ", ": ")))
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
+        self.num+=1
+        page = response.url
         heading = response.css('#firstHeading::text').get()
         body = response.css('#bodyContent p:not(.mw-empty-elt):nth-of-type(1n), #bodyContent p:not(.mw-empty-elt):nth-of-type(2n)').extract()
-        #bodyContent p:first-of-type
         soup = BeautifulSoup(body[0])
         overview = soup.get_text().encode("utf-8")
         overview = re.sub(r'\[.+?\]', '', overview)
@@ -30,14 +33,20 @@ class DiseaseSpider(scrapy.Spider):
             paragraph2 = soup.get_text().encode("utf-8")
             paragraph2 = re.sub(r'\[.+?\]', '', paragraph2)
             overview += paragraph2
-        self.diseases.append({"heading" : heading, "overview" : overview})
-        #print("\n\nheading: " + heading + "\n\n")
-        #print("\n\nbody: " + overview + "\n\n")
+        code = filter(lambda v: find_code(v, page),self.disease_urls)
+        self.f.write(json.dumps({"id": code[0]["id"], "heading" : heading, "overview" : overview}, indent=4, separators=(", ", ": ")))
+        if self.num < len(self.disease_urls):
+            self.f.write(',\n')
+        else:
+            self.f.write(']')
+        self.diseases.append({"id": code[0]["id"], "heading" : heading, "overview" : overview})
 
 def read_from_json():
     file_name = '../diseases.json'
     f = open(file_name, 'r')
     return json.loads(f.read())
-    
 
-
+def find_code(variable, url):
+    if(variable["wikipediaURL"] == url):
+        return True
+    return False
