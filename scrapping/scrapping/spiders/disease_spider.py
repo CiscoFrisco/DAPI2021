21a9e6f2213205e009321251d096125138c941c4
helenaMontenegro <up201604184@fe.up.pt>
2020-10-10 23:05:30 +0100

    Add json with scrapped disease info

--- scrapping/scrapping/spiders/disease_spider.py
+++ scrapping/scrapping/spiders/disease_spider.py
@@ -7,30 +7,37 @@ import os
 
 class DiseaseSpider(scrapy.Spider):
     name = "disease"
+    diseases = []
 
     def start_requests(self):
-        read_from_json()
-        urls = [
-            'https://en.wikipedia.org/wiki/Alzheimer%27s_disease',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        disease_urls = read_from_json()
+        for disease in disease_urls:
+            yield scrapy.Request(url=disease["wikipediaURL"], callback=self.parse)
+        print(len(self.diseases))
+        f = open("../disease_overviews.json", "w")
+        f.write(json.dumps(self.diseases, indent=4, separators=(", ", ": ")))
 
     def parse(self, response):
         page = response.url.split("/")[-2]
         heading = response.css('#firstHeading::text').get()
-        body = response.css('#mw-content-text table + p, #mw-content-text table + p + p').extract()
+        body = response.css('#bodyContent p:not(.mw-empty-elt):nth-of-type(1n), #bodyContent p:not(.mw-empty-elt):nth-of-type(2n)').extract()
+        #bodyContent p:first-of-type
         soup = BeautifulSoup(body[0])
         overview = soup.get_text().encode("utf-8")
         overview = re.sub(r'\[.+?\]', '', overview)
-        soup = BeautifulSoup(body[1])
-        paragraph2 = soup.get_text().encode("utf-8")
-        paragraph2 = re.sub(r'\[.+?\]', '', paragraph2)
-        overview += paragraph2
-        print("\n\nheading: " + heading + "\n\n")
-        print("\n\nbody: " + overview + "\n\n")
+        if (len(body) > 1):
+            soup = BeautifulSoup(body[1])
+            paragraph2 = soup.get_text().encode("utf-8")
+            paragraph2 = re.sub(r'\[.+?\]', '', paragraph2)
+            overview += paragraph2
+        self.diseases.append({"heading" : heading, "overview" : overview})
+        #print("\n\nheading: " + heading + "\n\n")
+        #print("\n\nbody: " + overview + "\n\n")
 
 def read_from_json():
-    file_name = '../Wikidata/diseases.json'
-    f = open(file_name, "r")
-    print(f.read())
+    file_name = '../diseases.json'
+    f = open(file_name, 'r')
+    return json.loads(f.read())
+    
+
+
