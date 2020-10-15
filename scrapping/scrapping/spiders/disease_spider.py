35436f141e7d43071f8199c5734cce3b4f5466e3
helenaMontenegro <up201604184@fe.up.pt>
2020-10-15 15:04:00 +0100

    Generate UPDATE statements to obtain overview of diseases and all other variables

--- scrapping/scrapping/spiders/disease_spider.py
+++ scrapping/scrapping/spiders/disease_spider.py
@@ -4,19 +4,21 @@ from bs4 import BeautifulSoup
 import re
 import json
 import os
+import unicodedata
 
 class DiseaseSpider(scrapy.Spider):
     name = "disease"
-    diseases = []
     disease_urls = []
     f = ''
+    sql_file = ''
     num = 0
 
     def start_requests(self):
         self.disease_urls = read_from_json()
         
         self.f = open("../disease_overviews.json", "w")
-        self.f.write('[')#json.dumps(self.diseases, indent=4, separators=(", ", ": "))
+        self.sql_file = open("disease_overviews.sql", "w")
+        self.f.write('[')
         for disease in self.disease_urls:
             yield scrapy.Request(url=disease["wikipediaURL"], callback=self.parse)
 
@@ -26,20 +28,22 @@ class DiseaseSpider(scrapy.Spider):
         heading = response.css('#firstHeading::text').get()
         body = response.css('#bodyContent p:not(.mw-empty-elt):nth-of-type(1n), #bodyContent p:not(.mw-empty-elt):nth-of-type(2n)').extract()
         soup = BeautifulSoup(body[0])
-        overview = soup.get_text().encode("utf-8")
+        overview = unicodedata.normalize("NFKD", soup.get_text(strip=True)).encode('ASCII', 'ignore')
         overview = re.sub(r'\[.+?\]', '', overview)
         if (len(body) > 1):
             soup = BeautifulSoup(body[1])
-            paragraph2 = soup.get_text().encode("utf-8")
+            paragraph2 = unicodedata.normalize("NFKD", soup.get_text(strip=True)).encode('ASCII', 'ignore')
             paragraph2 = re.sub(r'\[.+?\]', '', paragraph2)
             overview += paragraph2
         code = filter(lambda v: find_code(v, page),self.disease_urls)
+        overview = re.sub(r'\'', '\'\'', overview)
         self.f.write(json.dumps({"id": code[0]["id"], "heading" : heading, "overview" : overview}, indent=4, separators=(", ", ": ")))
+        sql_statement = 'UPDATE disease SET overview = \'' + overview + '\' WHERE id = \'' + code[0]["id"] + '\';\n'
+        self.sql_file.write(sql_statement)
         if self.num < len(self.disease_urls):
             self.f.write(',\n')
         else:
             self.f.write(']')
-        self.diseases.append({"id": code[0]["id"], "heading" : heading, "overview" : overview})
 
 def read_from_json():
     file_name = '../diseases.json'
@@ -48,5 +52,10 @@ def read_from_json():
 
 def find_code(variable, url):
     if(variable["wikipediaURL"] == url):
+        return True
+    return False
+
+def find_id(variable, id):
+    if(variable["id"] == id):
         return True
     return False
