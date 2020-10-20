b1e3d7072b11fc3ae5ab03c15a829e687af51816
helenaMontenegro <up201604184@fe.up.pt>
2020-10-20 19:48:51 +0100

    Add R file

---
+++ Data Exploration/data_characterization.R
@@ -0,0 +1,3 @@
+Length <- OverviewSizes$length;
+h <- hist(Length, main="Overview text size", xlab="Number of characters", col="deepskyblue", ylim=c(0, 400), breaks=seq(0,2600,200), xaxp=c(0,2600,13));
+text(h$mids, h$counts, labels=h$counts, adj=c(0.5,-0.5))
